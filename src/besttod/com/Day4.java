dc6ba731d2da91c7b02816925da6b5f1a95d6c66
besttodd <besttodd@hotmail.com>
2020-12-11 16:31:56 +0000

    Advent of Code 2020 https://adventofcode.com/ Day 4 complete

--- src/besttod/com/Day4.java
+++ src/besttod/com/Day4.java
@@ -31,6 +31,7 @@ import java.util.*;
 //                pid (Passport ID) - a nine-digit number, including leading zeroes.
 //                cid (Country ID) - ignored, missing or not.
 //                Count the passports where all required fields are both present and valid according to the above rules.
+//Correct answer: 188
 
 public class Day4 {
     private final static String FILEPATH = "C:\\Users\\bestt\\Coding\\AdventOfCode\\day4_input.txt";
@@ -43,37 +44,101 @@ public class Day4 {
         String[] pairValues;
         HashMap<String, String> keyPairs = new HashMap<>();
 
-
         for (String[] passport : passports) { //Each passport
-            for (String line : passport) { //Each line of the passport
-                //System.out.println(line);
-                String[] fields = line.split(" ");
-                for (String field : fields) { //Each field on the line
-                    pairValues = field.split(":");
-                    pairValues[0] = checkString(pairValues[0]);
-                    keyPairs.put(pairValues[0], pairValues[1]);
+            for (int j = 0; j < passport.length; j++) { //Each line of the passport
+                String[] fields = passport[j].split(" ");
+                if (!fields[0].matches("\\?|valid|invalid")) {
+                    for (int k = 0; k < fields.length; k++) { //Each field on the line
+                        pairValues = fields[k].split(":");
+                        pairValues[0] = checkField(pairValues[0]);
+                        if (checkValue(pairValues).equals("invalid")) {
+                            passport[passport.length - 1] = "invalid";
+                            k = fields.length;
+                            j = passport.length;
+                        } else {
+                            keyPairs.put(pairValues[0], pairValues[1]);
+                            passport[passport.length - 1] = "valid";
+                        }
+                    }
                 }
             }
-
-            System.out.println(keyPairs);
             if (keyPairs.size() == 8) {
                 validPassports++;
             } else if (keyPairs.size() == 7 && !keyPairs.containsKey("cid")) {
                 validPassports++;
-            }
+            } else passport[passport.length - 1] = "invalid";
+
+            System.out.println(keyPairs);
+            System.out.println(passport[passport.length - 1]);
 
             keyPairs.clear();
             System.out.println("===== Next Passport =====");
         }
 
-        System.out.println("Total Passports: " + passports.size() + " Valid Passports:" + validPassports);
+        System.out.println("\nTotal Passports: " + passports.size() + " Valid Passports:" + validPassports);
     }
 
-    /*private String checkValue(String value) {
-        return value;
-    }*/
+    private String checkValue(String[] pair) {
+        switch (pair[0]) {
+            case "byr":
+                if (Integer.parseInt(pair[1]) >= 1920 && Integer.parseInt(pair[1]) <= 2003) {
+                    return "valid";
+                } else {
+                    return "invalid";
+                }
+            case "iyr":
+                if (Integer.parseInt(pair[1]) >= 2010 && Integer.parseInt(pair[1]) <= 2020) {
+                    return "valid";
+                } else {
+                    return "invalid";
+                }
+            case "eyr":
+                if (Integer.parseInt(pair[1]) >= 2020 && Integer.parseInt(pair[1]) <= 2030) {
+                    return "valid";
+                } else {
+                    return "invalid";
+                }
+            case "hgt":
+                String[] height = pair[1].split("cm|in");
+                if (pair[1].contains("cm")) {
+                    if (Integer.parseInt(height[0]) >= 150 && Integer.parseInt(height[0]) <= 193) {
+                        return "valid";
+                    } else return "invalid";
+                } else if (Integer.parseInt(height[0]) >= 59 && Integer.parseInt(height[0]) <= 76) {
+                    return "valid";
+                } else return "invalid";
+            case "hcl":
+                if (pair[1].contains("#") && pair[1].length() == 7) {
+                    String numbers = pair[1].substring(1);
+                    if (numbers.matches("[a-fA-F0-9]+")) {
+                        return "valid";
+                    }
+                } else return "invalid";
+            case "ecl":
+                switch (pair[1].toLowerCase()) {
+                    case "amb":
+                    case "blu":
+                    case "brn":
+                    case "gry":
+                    case "grn":
+                    case "hzl":
+                    case "oth":
+                        return "valid";
+                    default:
+                        return "invalid";
+                }
+            case "pid":
+                if (pair[1].matches("[0-9]+") && pair[1].length() == 9) {
+                    return "valid";
+                } else return "invalid";
+            case "cid":
+                return "valid";
+            default:
+                return "invalid";
+        }
+    }
 
-    private String checkString(String string) {
+    private String checkField(String string) {
         switch (string) {
             case "byr":
             case "iyr":
@@ -98,14 +163,16 @@ public class Day4 {
                 if (!nextLine.isEmpty()) {
                     lines.add(nextLine);
                 } else {
-                    String[] passportEntries = new String[lines.size()];
+                    String[] passportEntries = new String[lines.size() + 1];
                     lines.toArray(passportEntries);
+                    passportEntries[passportEntries.length - 1] = "?";
                     passports.add(passportEntries);
                     lines.clear();
                 }
             }
-            String[] lastEntry = new String[lines.size()];
+            String[] lastEntry = new String[lines.size() + 1];
             lines.toArray(lastEntry);
+            lastEntry[lastEntry.length - 1] = "?";
             passports.add(lastEntry);
             lines.clear();
             br.close();
