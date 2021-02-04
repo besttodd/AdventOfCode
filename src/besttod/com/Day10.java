dcc00bc58028489f8040d4a8e51520bc17a9dd0d
besttodd <besttodd@hotmail.com>
2021-02-04 09:21:24 +0000

    Advent of Code 2020 https://adventofcode.com/ Day 10 part 2 complete

--- src/besttod/com/Day10.java
+++ src/besttod/com/Day10.java
@@ -13,13 +13,14 @@ package besttod.com;
 //Correct answer: 2312.
 
 //         Part 2: What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
-//Correct answer:
+//Correct answer: 12089663946752
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Day10 {
     private final static String FILEPATH = "C:\\Users\\bestt\\Coding\\AdventOfCode\\day10_input.txt";
+    private final static int MIN_JOLTS = 3;
 
     public Day10() {
         List<String> input = new ReadFile().readFile(FILEPATH);
@@ -28,30 +29,31 @@ public class Day10 {
         for (String s : input) {
             intInput.add(Integer.parseInt(s));
         }
-        intInput.add(findHighest(intInput) + 3);
+        intInput.add(findHighest(intInput) + MIN_JOLTS);
         System.out.println("Built-In: " + intInput.get(intInput.size() - 1));
 
-        List<Integer> order = new ArrayList<>();
+        List<Integer> ordered = new ArrayList<>();
+
         int joltage = 0;
         int next = 1;
         int diffOf1 = 0;
-        int diffOf2 = 0;
+        //int diffOf2 = 0;
         int diffOf3 = 0;
 
         while (joltage < findHighest(intInput)) {
             if (intInput.contains(next)) {
                 joltage = intInput.get(intInput.indexOf(next));
-                order.add(joltage);
+                ordered.add(joltage);
                 diffOf1++;
                 next++;
             } else if (intInput.contains(next + 1)) {
                 joltage = intInput.get(intInput.indexOf(next + 1));
-                order.add(joltage);
-                diffOf2++;
+                ordered.add(joltage);
+                //diffOf2++;
                 next += 2;
             } else if (intInput.contains(next + 2)) {
                 joltage = intInput.get(intInput.indexOf(next + 2));
-                order.add(joltage);
+                ordered.add(joltage);
                 diffOf3++;
                 next += 3;
             }
@@ -59,6 +61,24 @@ public class Day10 {
 
         System.out.println(diffOf1 + " differences of 1 jolt & " + diffOf3 + " differenecs of 3 jolts.");
         System.out.println("Answer: " + diffOf1 * diffOf3);
+        //System.out.println(ordered);
+
+        //PART 2
+        long count = getCombinations(ordered.stream().mapToInt(Integer::intValue).toArray());
+        System.out.println("\nDistinct Arrangements: " + count);
+    }
+
+    private long getCombinations(int[] list) {
+        long[] sums = new long[list[list.length - 1] + 1];
+        sums[0] = 1;
+        for (int j : list) {
+            final long x = j >= 3 ? sums[j - 3] : 0;
+            final long y = j >= 2 ? sums[j - 2] : 0;
+            final long z = j >= 1 ? sums[j - 1] : 0;
+            sums[j] = x + y + z;
+        }
+
+        return sums[sums.length - 1];
     }
 
     private int findHighest(List<Integer> list) {
